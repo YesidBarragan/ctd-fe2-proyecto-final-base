201adbe5ace167efeb0b2ef239c382f1a880b691
YesidBarragan <yesarmand2006@gmail.com>
2022-07-02 17:57:12 -0500

    hotfix: citaAPI eslint

--- src/features/quote/citaAPI.ts
+++ src/features/quote/citaAPI.ts
@@ -1,11 +1,9 @@
-import { API_URL } from "../../app/constants";
-import { ICita } from "./types";
+import { API_URL } from '../../app/constants';
+import { ICita } from './types';
 
-export const obtenerCita: (personaje?: string) => Promise<ICita> = async (
-  personaje
-) => {
+const obtenerCita: (personaje?: string) => Promise<ICita> = async (personaje) => {
   if (personaje && parseInt(personaje)) {
-    throw new Error("El nombre debe ser un texto");
+    throw new Error('El nombre debe ser un texto');
   }
 
   const url = personaje ? `${API_URL}?character=${personaje}` : API_URL;
@@ -21,3 +19,5 @@ export const obtenerCita: (personaje?: string) => Promise<ICita> = async (
 
   return dataNormalizada;
 };
+
+export default obtenerCita;
